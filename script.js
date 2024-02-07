d4e928e534401ceebe2d16ced3b7c8224c3ff4c0
ifabuma <153803804+ifabuma@users.noreply.github.com>
2024-02-07 18:57:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('EgipciaAR').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Egipcia/';
+});
+
+document.getElementById('EgipciaPDF').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Egipcia/';
+});
+
+document.getElementById('Griega').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Griega/';
+ });
+
+document.getElementById('GriegaPDF').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Griega/';
+});
+
+document.getElementById('TornoAR').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Torno-de-Pedal-y-Pertiga/';
+ });
+
+document.getElementById('TornoPDF').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Torno-de-Pedal-y-Pertiga/';
+});
+
+document.getElementById('MedievalAR').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Medieval/';
+ });
+
+document.getElementById('MedievalPDF').addEventListener('click', function() {
+  window.location.href = 'https://ifabuma.github.io/Grua-Medieval/';
+ });
+
